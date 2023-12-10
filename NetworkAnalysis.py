8cb624e9bdb03bba798576c064849068f2d4ce6d
aliomran10 <125608514+aliomran10@users.noreply.github.com>
2023-12-10 20:23:18 +0200

    NetworkAnalysis.py

--- NetworkAnalysis.py
+++ NetworkAnalysis.py
@@ -1,51 +1,71 @@
 # 1. Most Influential Users (Most Followers)
 def most_influential(graph):
-    out_degrees = dict(graph.out_degree())
-    max_out_degree = max(out_degrees.values())
-    most_influential_users = [user for user, out_degree in out_degrees.items() if out_degree == max_out_degree]
+    user_ids = []
+    out_degrees = []
+
+    for user_id in graph.nodes:
+        user_ids.append(user_id)
+        out_degrees.append(len(list(graph.successors(user_id))))
+
+    max_out_degree = max(out_degrees)
+
+    most_influential_users = [user_ids[i] for i, degree in enumerate(out_degrees) if degree == max_out_degree]
 
     print(f"The most influential users are: {most_influential_users}")
-    return
+    return most_influential_users
 
 
 # 2. Most Active User (Connected to Lots of Users)
 def most_active(graph):
-    total_edges = dict(graph.degree())
-    max_total_edges = max(total_edges.values())
-    most_active_users = [user for user, degree in total_edges.items() if degree == max_total_edges]
+    user_ids = []
+    total_edges = []
+
+    for user_id in graph.nodes:
+        user_ids.append(user_id)
+        total_edges.append(graph.degree(user_id))
+
+    max_total_edges = max(total_edges)
+
+    most_active_users = [user_ids[i] for i, degree in enumerate(total_edges) if degree == max_total_edges]
 
     print(f"The most active users are: {most_active_users}")
-    return
+    return most_active_users
 
 
 # 3.  Mutual followers between 2 users
 def mutual_followers(graph, node1, node2):
-    if node1 not in graph or node2 not in graph:
+    if node1 not in graph.nodes or node2 not in graph.nodes:
         print("Invalid nodes. Please provide existing nodes.")
         return None
 
-    followers_node1 = set(graph.successors(node1))
-    followers_node2 = set(graph.successors(node2))
+    followers_node1 = []
+    for successor in graph.successors(node1):
+        followers_node1.append(successor)
 
-    mut_followers = followers_node1.intersection(followers_node2)
+    followers_node2 = []
+    for successor in graph.successors(node2):
+        followers_node2.append(successor)
+
+    mut_followers = [follower for follower in followers_node1 if follower in followers_node2]
     print(f"Mutual followers between nodes {node1} and {node2}: {mut_followers}")
-    return list(mut_followers)
+    return mut_followers
 
 
 # 4.  Suggest users to follow
 def suggest_followers(graph):
-    suggested_followers_dict = {}
+    suggested_followers = []
 
     for user in graph.nodes:
-        followers_of_followers = set()
+        user_suggestions = []
 
         for successor in graph.successors(user):
-            followers_of_followers.update(graph.successors(successor))
+            for suggested_follower in graph.successors(successor):
+                if suggested_follower not in graph.predecessors(user) and suggested_follower != user and suggested_follower not in user_suggestions:
+                    user_suggestions.append(suggested_follower)
 
-        suggested_followers = followers_of_followers - set(graph.predecessors(user)) - {user}
-        suggested_followers_dict[user] = list(suggested_followers)
+        suggested_followers.append((user, user_suggestions))
 
-    for user, suggested_followers in suggested_followers_dict.items():
-        print(f"Suggested followers for User {user}: {suggested_followers}")
+    for user, user_suggestions in suggested_followers:
+        print(f"Suggested followers for User {user}: {user_suggestions}")
 
-    return suggested_followers_dict
+    return suggested_followers
